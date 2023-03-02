956ddd37d3d501f312d099b4c2dcd9ae74f9d827
rizkdfd <rizkadfd@it.student.pens.ac.id>
2023-03-02 09:08:09 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Demo'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text('Hello Worlds'),
         ),
       )
     );
